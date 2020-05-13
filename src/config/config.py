ecc2d89878fac8bc0c8390f8b4f33d5f19e9c38b
Anyesh <sthacruz@gmail.com>
2020-05-13 17:15:15 +0545

    Add a configurations file

---
+++ src/config/config.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+DATA_PATH = os.path.join(BASE_DIR, "data")
+
+DATASET_NAME = "ISEAR_dataset.csv"
+
+DATASET_URL = "https://www.floydhub.com/api/v1/resources/qM4BHN3pNjkfkvYjMMtjU4/ISEAR.csv?content=true&rename=isearcsv"
+
+MODEL_PATH = os.path.join(BASE_DIR, "models")
+
+CHECKPOINT_PATH = os.path.join(BASE_DIR, "checkpoints")
+
+VOCAB_SIZE = 3000
