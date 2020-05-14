8f467288bb72447dc5c53411b9d73fbc497d9839
Anyesh <sthacruz@gmail.com>
2020-05-14 16:16:10 +0545

    Make modular process data function

--- src/features/feature_generator.py
+++ src/features/feature_generator.py
@@ -3,13 +3,16 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 from tensorflow.keras.preprocessing import text
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
 from .text_processors import KerasTextPreprocessor, TFIDFProcessor
-from src.config import config
+from sklearn.model_selection import train_test_split
+from src.utils.tokenizer import nltk_tokenizer_df
 import pickle
 
 
-def process_data(VOCAB_SIZE, processor_engine="keras", train_size=0.8):
+def process_data(
+    dataset_path, dataset_name, vocab_size, processor_engine="KERAS", train_size=0.8,
+):
 
     """ Process the text data with respect to the processor_engine
 
@@ -27,68 +30,59 @@ def process_data(VOCAB_SIZE, processor_engine="keras", train_size=0.8):
     """
 
     df = pd.read_csv(
-        os.path.join(config.DATA_PATH, config.DATASET_NAME),
+        os.path.join(dataset_path, dataset_name),
         names=["#", "emotions", "texts"],
         header=None,
     )
-    data = df.dropna()
-    data = shuffle(data, random_state=0)
 
-    _train_size = int(len(data) * train_size)
+    # df["texts"] = df["texts"].apply(nltk_tokenizer_df)
+    data = df.dropna()
+    # data = shuffle(data, random_state=32)
+    # _train_size = int(len(data) * train_size)
 
     print(f"[INFO] Splitting data of size {len(data)} by {train_size} train size..")
 
-    train_data = data["texts"].values[:_train_size]
-    test_data = data["texts"].values[_train_size:]
+    # train_data = data["texts"].values[:_train_size]
+    # test_data = data["texts"].values[_train_size:]
+    labels = data["emotions"].values
+    label_encoder = LabelEncoder()
+    label_encoded = label_encoder.fit_transform(labels)
 
-    print(f"[INFO] Train size:{train_data.shape} & Test size: {test_data.shape}")
+    X_train, X_test, y_train, y_test = train_test_split(
+        data["texts"],
+        label_encoded,
+        shuffle=True,
+        train_size=train_size,
+        random_state=32,
+    )
 
-    if processor_engine == "keras":
+    print(f"[INFO] Train size:{X_train.shape} & Test size: {X_test.shape}")
 
-        processor = KerasTextPreprocessor(VOCAB_SIZE)
+    if processor_engine == "KERAS":
 
-        ## Keras does not have stop words remover
-        ## so passing dataframe instead values to process
-        processor.create_tokenizer(data["texts"])
+        processor = KerasTextPreprocessor(vocab_size)
 
-        body_train = processor.transform_text(train_data)
-        body_test = processor.transform_text(test_data)
+        processor.create_tokenizer(data["texts"].values)
 
-    elif processor_engine == "tfidf":
-        processor = TFIDFProcessor(feature_size=VOCAB_SIZE)
+        train_data = processor.transform_text(X_train)
+        test_data = processor.transform_text(X_test)
+
+    elif processor_engine == "SKLEARN":
+        processor = TFIDFProcessor(feature_size=vocab_size)
 
         processor.create_vocab(data["texts"].values)
 
-        body_train = processor.transform_text(train_data)
-        body_test = processor.transform_text(test_data)
+        train_data = processor.transform_text(X_train)
+        test_data = processor.transform_text(X_test)
 
     else:
         raise "[ERROR] No processor engine!!"
 
-    labels_split = [labels.split(",") for labels in data["emotions"].values]
+    # train_labels = label_encoded[:_train_size]
+    # test_labels = label_encoded[_train_size:]
 
-    label_encoder = MultiLabelBinarizer()
-    label_encoded = label_encoder.fit_transform(labels_split)
-
-    train_labels = label_encoded[:_train_size]
-    test_labels = label_encoded[_train_size:]
-
-    print("[INFO] Saving processor..")
-    with open(
-        os.path.join(
-            config.CHECKPOINT_PATH, f"{processor_engine}-data_processor_state.pkl"
-        ),
-        "wb",
-    ) as f:
-        pickle.dump(processor, f)
-
-    print("[INFO] Saved data processor..")
-    with open(os.path.join(config.CHECKPOINT_PATH, "label_encoder.pkl"), "wb") as f:
-        pickle.dump(label_encoder, f)
-    print("[INFO] Saved label encoder..")
-
-    return body_train, train_labels, body_test, test_labels
+    return train_data, y_train, test_data, y_test, processor, label_encoder
 
 
 if __name__ == "__main__":
-    process_data(5000, processor_engine="tfidf")
+    process_data(5000, processor_engine="SKLEARN")
