497d1274cf91eccf9c965216edc302609c9f460d
Anyesh <sthacruz@gmail.com>
2020-05-14 20:50:44 +0545

    Add temp main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
 from .features.feature_generator import process_data
 from .models.train_model import TrainModel
-from .models.test_model import TestModel
+
+# from .models.test_model import TestModel
 from .config import config
 from sklearn.metrics import accuracy_score, f1_score
 from .utils.serializer import save_object
 import numpy as np
 import os
+import mlflow
+import mlflow.sklearn
 
 
-def main():
+VOCAB_SIZE = 2000
+MODEL_NAME = "xgboost"
+processor_engine = "SKLEARN"
 
-    VOCAB_SIZE = 400
-    MODEL_NAME = "naive_bayes"
-    processor_engine = "SKLEARN"
-    model_params = {"model_name": MODEL_NAME, "vocab_size": VOCAB_SIZE}
+
+if __name__ == "__main__":
+    model_params = {"model_name": MODEL_NAME, "vocab_size": VOCAB_SIZE, "gpu_id": 0}
+
+    mlflow.log_params(model_params)
 
     (
         train_data,
@@ -39,11 +45,19 @@ def main():
 
     trainer = TrainModel.sklearn(**model_params)
     clf, history = trainer.train(train_data, train_label)
-    test_clf = TestModel.from_path(clf)
-    predictions = test_clf.predict(test_data)
+    print(clf)
+    # test_clf = TestModel.from_path(clf)
+    predictions = clf.predict(test_data)
 
-    print("Accuracy score: ", accuracy_score(predictions, test_label))
-    print("F1 score: ", f1_score(predictions, test_label, average="weighted"))
+    accuracy_score = accuracy_score(predictions, test_label)
+    f1_score = f1_score(predictions, test_label, average="weighted")
+
+    print("Accuracy score: ", accuracy_score)
+    print("F1 score: ", f1_score)
+
+    mlflow.log_metric("Accuracy score", accuracy_score)
+    mlflow.log_metric("F1 score", f1_score)
+    mlflow.sklearn.log_model(clf, MODEL_NAME)
 
     np.save(
         os.path.join(
@@ -53,7 +67,3 @@ def main():
         ),
         [train_data, train_label, test_data, test_label],
     )
-
-
-if __name__ == "__main__":
-    main()
