635990ad1b84d146cdbbd6ca24c813b6363cdc1c
Anyesh <sthacruz@gmail.com>
2020-05-14 16:17:01 +0545

    Create class for model testing

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -1,22 +1,42 @@
 import pickle
 import os
 
+from src.config import config
+
 
 class TestModel(object):
-    def __init__(self, model, processor):
+    def __init__(self, model, processor, use_processor):
         self._model = model
         self._processor = processor
+        self._use_processor = use_processor
 
-    def predict(self, instances, **kwargs):
-        processed_data = self._processor.transform_text(instances)
+    def predict(self, instances):
+        processed_data = instances
+
+        if self._use_processor:
+            processed_data = self._processor.transform_text(instances)
+
         predictions = self._model.predict(processed_data)
-        return predictions.tolist()
+        return predictions
 
     @classmethod
-    def from_path(cls, mode_dir):
-        import tensorflow.keras as keras
+    def from_path(cls, model, processor_path=None, use_processor=False):
 
-        model = keras.models.load_model(os.path.join("checkpoints", "classifier.h5"))
-        with open(os.path.join("checkpoints", "processor_state.pkl"), "rb") as f:
-            processor = pickle.load(f)
-        return cls(model, processor)
+        processor = None
+
+        if use_processor:
+            with open(processor_path, "rb",) as f:
+                processor = pickle.load(f)
+
+        return cls(model, processor, use_processor)
+
+    # @classmethod
+    # def keras(cls, model_path):
+    #     import tensorflow.keras as keras
+
+    #     model = keras.models.load_model(model_path)
+    #     with open(
+    #         os.path.join(config.CHECKPOINT_PATH, "keras-data_processor_state.pkl"), "rb"
+    #     ) as f:
+    #         processor = pickle.load(f)
+    #     return cls(model, processor)
