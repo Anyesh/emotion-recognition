85e315b8e24a5f9b4798e4513c095147b44c5202
Anyesh <sthacruz@gmail.com>
2020-05-14 16:16:27 +0545

    Remove tokenizer from text_processors

--- src/features/text_processors.py
+++ src/features/text_processors.py
@@ -1,8 +1,7 @@
 from tensorflow.keras.preprocessing import text
 from sklearn.feature_extraction.text import TfidfVectorizer
-from src.utils.tokenizer import nltk_tokenizer, nltk_tokenizer_keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-
 import pickle
 import os
 
@@ -12,17 +11,19 @@ class KerasTextPreprocessor:
         self._vocab_size = vocab_size
         self._tokenizer = None
 
-    def create_tokenizer(self, df_corpus):
+    def create_tokenizer(self, corpus):
 
-        ## Removing stopwords using nltk
-        clean_corpus = df_corpus.apply(nltk_tokenizer_keras)
         tokenizer = text.Tokenizer(num_words=self._vocab_size)
-        tokenizer.fit_on_texts(clean_corpus.values)
+        tokenizer.fit_on_texts(corpus)
         self._tokenizer = tokenizer
 
     def transform_text(self, text_list):
-        text_matrix = self._tokenizer.texts_to_matrix(text_list)
-        return text_matrix
+        text_sequence = self._tokenizer.texts_to_sequences(text_list)
+        padded_sequence = pad_sequences(
+            text_sequence, maxlen=self._vocab_size, padding="post"
+        )
+
+        return padded_sequence
 
 
 class TFIDFProcessor:
@@ -31,19 +32,15 @@ class TFIDFProcessor:
     ):
 
         self._feature_size = feature_size
-        self._vocabulary = None
+        self._vectorizer = None
         self._n_grams = n_grams
         self._use_n_grams = use_n_grams
 
-        self._tokenizer = nltk_tokenizer
-
     def create_vocab(self, corpus):
-        _vectorizer = TfidfVectorizer(
-            tokenizer=self._tokenizer, max_features=self._feature_size,
-        )
+        _vectorizer = TfidfVectorizer(max_features=self._feature_size)
 
         self._vectorizer = _vectorizer.fit(corpus)
-        self._vocabulary = self._vectorizer.vocabulary_
+        # self._vocabulary = self._vectorizer.vocabulary_
 
     def transform_text(self, text_list):
 
