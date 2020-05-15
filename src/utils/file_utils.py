1afae931b02351bdb4c9a4bddb939fff248dd599
Anyesh <sthacruz@gmail.com>
2020-05-15 12:15:06 +0545

    Move from src to emotion_detection

--- src/utils/file_utils.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-
-
-def get_latest_file(dir_path, file_type="*"):
-    list_of_files = glob.glob(os.path.join(dir_path, file_type))
-    latest_file = max(list_of_files, key=os.path.getctime)
-    return latest_file
